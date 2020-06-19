b21a3a1bf5741e04e6b03aa203aa3b5a284c0c1e
Boogy83 <bozhidar.atanasov@abv.bg>
2020-06-19 15:57:43 +0300

    ...

--- config/config.js
+++ config/config.js
@@ -3,10 +3,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        // dbURL: 'mongodb://localhost:27017/cubes'
+        dbURL: 'mongodb://localhost:27017/cubes'
     },
     production: {
         port: process.env.PORT || 3000,
+        dbURL: 'mongodb+srv://Boogy:131311@boogy-xm4wc.mongodb.net/Boogy?retryWrites=true&w=majority'
     }
 };
 
