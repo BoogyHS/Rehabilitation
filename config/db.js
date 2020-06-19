ff807348fd8ae1a9844b245cf066059518111aa0
Boogy83 <bozhidar.atanasov@abv.bg>
2020-06-19 15:27:29 +0300

    ...

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
-const config = require('./config');
-const mongoose = require('mongoose');
+// const config = require('./config');
+// const mongoose = require('mongoose');
 
-module.exports = () => {
-  return mongoose.connect(config.dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  });
-};
+// module.exports = () => {
+//   return mongoose.connect(config.dbURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+//   });
+// };
