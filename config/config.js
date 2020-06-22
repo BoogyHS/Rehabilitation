1f3955032887c92b83f3a5910d97170080bf0ece
Boogy83 <bozhidar.atanasov@abv.bg>
2020-06-22 14:03:50 +0300

    create gitattributes

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        dbURL: 'mongodb://localhost:27017/cubes'
+        dbURL: 'mongodb://localhost:27017/rehabilitation'
     },
     production: {
         port: process.env.PORT || 3000,
