c881d9253be87ab34befbf69c3d53d0fdb2911ae
Boogy83 <bozhidar.atanasov@abv.bg>
2020-07-14 17:29:07 +0300

    edit

--- config/mongoonline.js
+++ config/mongoonline.js
@@ -1,10 +1,2 @@
 
-// const MongoClient = require('mongodb').MongoClient;
-// //Replace <password> with the password for the Boogy user.
-// const uri = "mongodb+srv://Boogy:131311@boogy-xm4wc.mongodb.net/Boogy?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
